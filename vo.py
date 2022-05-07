54ec162d786e52fa54a3098c0a38dba3185ad21d
shiannn <b05502087@ntu.edu.tw>
2022-05-07 16:46:53 +0800

    commit correspondence

--- vo.py
+++ vo.py
@@ -15,6 +15,8 @@ class SimpleVO:
         
         self.frame_paths = sorted(list(glob.glob(os.path.join(args.input, '*.png'))))
 
+        self.args = args
+
     def run(self):
         vis = o3d.visualization.Visualizer()
         vis.create_window()
@@ -64,9 +66,9 @@ class SimpleVO:
     def get_rescale_ratio(self, pre_points3D, points3D):
         assert points3D.shape[1] == 4 and pre_points3D.shape[1] == 4
         ### points3D (homogeneous) Nx4
-        min_idx = min(pre_points3D.shape[0], points3D.shape[0])
-        pre_points3D = pre_points3D[:min_idx]
-        points3D = points3D[:min_idx]
+        # min_idx = min(pre_points3D.shape[0], points3D.shape[0])
+        # pre_points3D = pre_points3D[:min_idx]
+        # points3D = points3D[:min_idx]
         
         #sel = np.random.randint(low=0,high=pre_points3D.shape[0], size=2)
         ### ratio
@@ -89,31 +91,40 @@ class SimpleVO:
         # upper = np.linalg.norm(sel_pts[0]-sel_pts[1], ord=2)
         # ratio = upper / lower
         return ratio
-                
+    
+    def get_rescale_ratio_correspond(self, pre_points3D, points3D):
+        assert pre_points3D.shape[0] == points3D.shape[0]
+        pd3D_pre = scipy.spatial.distance.pdist(pre_points3D)
+        pd3D = scipy.spatial.distance.pdist(points3D)
+        # print(pd3D_pre)
+        # print(pd3D)
+        # print(pd3D / pd3D_pre)
+        #ratio = np.median(pd3D / pd3D_pre)
+        ratio = np.median(pd3D) / np.median(pd3D_pre)
+        return ratio
+
     def process_frames(self, queue):
         R, t = np.eye(3, dtype=np.float64), np.zeros((3, 1), dtype=np.float64)
         ### Intrinsic K 3x3
         #img_pre = cv.imread(self.frame_paths[0])
         pre_norm = 1.0
-        for idx, _ in enumerate(self.frame_paths[1:], start=1):
+        for idx, _ in enumerate(self.frame_paths[1:-1], start=1):
             pre_frame = cv.imread(self.frame_paths[idx-1])
             cur_frame = cv.imread(self.frame_paths[idx])
             pos_frame = cv.imread(self.frame_paths[idx+1])
 
             #TODO: compute camera pose here
-            points01_pre, points01_cur, _, _, good_matches01 = get_correspondences(pre_frame, cur_frame, method='orb')
+            points01_pre, points01_cur, kp01_pre, kp01_cur, good_matches01 = get_correspondences(pre_frame, cur_frame, method='orb')
             es_Mat01, mask01 = cv.findEssentialMat(points01_cur, points01_pre, cameraMatrix=self.K, method=cv.RANSAC)
             _, R01, t01, _ = cv.recoverPose(es_Mat01, points01_cur, points01_pre, cameraMatrix=self.K)
 
-            points12_cur, points12_pos, _, _, good_matches12 = get_correspondences(cur_frame, pos_frame, method='orb')
+            points12_cur, points12_pos, kp12_cur, kp12_pos, good_matches12 = get_correspondences(cur_frame, pos_frame, method='orb')
             es_Mat12, mask12 = cv.findEssentialMat(points12_cur, points12_pos, cameraMatrix=self.K, method=cv.RANSAC)
             _, R12, t12, _ = cv.recoverPose(es_Mat12, points12_cur, points12_pos, cameraMatrix=self.K)
             
-            #match_pairs = np.array([[m.queryIdx, m.trainIdx] for m in good_matches])
 
             ### rescale t
-            #ratio = 1.01 if np.random.rand() > 0.5 else 0.99
-            if True:
+            if self.args.rescale_method == 'pairwise3D':
                 points3D01 = self.reproject_3D(
                     self.K, R01, t01,
                     points01_cur,
@@ -126,6 +137,24 @@ class SimpleVO:
                     points12_pos
                 )
                 ratio = self.get_rescale_ratio(points3D01, points3D12)    
+            elif self.args.rescale_method == 'correspond':
+                match_pairs01 = np.array([[m.queryIdx, m.trainIdx] for m in good_matches01])
+                match_pairs12 = np.array([[m.queryIdx, m.trainIdx] for m in good_matches12])
+                
+                inter_match, _, _ = np.intersect1d(match_pairs01[:,1], match_pairs12[:,0], return_indices=True)
+                assert inter_match.shape[0] > 0
+                kp01_pre = self.kp2array(kp01_pre)
+                kp01_cur = self.kp2array(kp01_cur)
+                
+                kp12_cur = self.kp2array(kp12_cur)
+                kp12_pos = self.kp2array(kp12_pos)
+
+                point3D01 = self.reproject_3D(self.K, R01, t01, kp01_cur[inter_match], kp01_pre[inter_match])
+                point3D12 = self.reproject_3D(self.K, R12, t12, kp12_cur[inter_match], kp12_pos[inter_match])
+                # print(point3D01.shape)
+                # print(point3D12.shape)
+                ratio = self.get_rescale_ratio_correspond(point3D01, point3D12)
+                
             print(ratio)
             ### accumulate scale
             dt = ratio* pre_norm* t12
@@ -135,6 +164,11 @@ class SimpleVO:
             queue.put((R, t))
 
             img = cur_frame
+            img[kp01_cur[:,1].astype(int), kp01_cur[:,0].astype(int)] = np.array([0,255,0])
+            img[kp01_cur[:,1].astype(int)-1, kp01_cur[:,0].astype(int)] = np.array([0,255,0])
+            img[kp01_cur[:,1].astype(int)+1, kp01_cur[:,0].astype(int)] = np.array([0,255,0])
+            img[kp01_cur[:,1].astype(int), kp01_cur[:,0].astype(int)-1] = np.array([0,255,0])
+            img[kp01_cur[:,1].astype(int), kp01_cur[:,0].astype(int)+1] = np.array([0,255,0])
             pre_norm = np.linalg.norm(dt, ord=2)
             cv.imshow('frame', img)
             if cv.waitKey(30) == 27: break
@@ -143,6 +177,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('input', help='directory of sequential frames')
     parser.add_argument('--camera_parameters', default='camera_parameters.npy', help='npy file of camera parameters')
+    parser.add_argument('-r', '--rescale_method', choices=['correspond','pairwise3D'], help='rescale method')
     args = parser.parse_args()
 
     vo = SimpleVO(args)
